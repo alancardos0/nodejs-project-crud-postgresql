f93e092d3471b511c8f145eec18180953652e364
Cardoso <92821038+alancardos0@users.noreply.github.com>
2023-01-07 18:41:11 -0300

    refatoring code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express, {Request,Response,NextFunction} from 'express';
+import express from 'express';
+import statusRouter from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();
 
+app.use(express.json());
+app.use(statusRouter);
 app.use(usersRoute);
 
-app.listen(3000 , () => console.log('Server rodando na porta 3000'));
+app.listen(3000, () => console.log('Server rodando na porta 3000'));
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,13 +1,32 @@
-import { Router,Request,Response,NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
-usersRoute.get('/users',(req:Request, res:Response, next:NextFunction) =>{
-    const users = [{
-      "name":"Alan",
-      "age":"19"
-    }];
-    res.status(200).send(users);
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+  const users = [{
+    "name": "Alan",
+    "age": "19"
+  }];
+  res.status(StatusCodes.OK).send(users);
 });
 
+usersRoute.get('/users/:id', (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+  const userId = req.params.id;
+})
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:id', (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+  const userId = req.params.id;
+  res.send(StatusCodes.OK).send(userId)
+});
+
+usersRoute.delete('/users/:id', (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+  const userId = req.params.id;
+  res.send(StatusCodes.OK).send("Deletado com sucesso");
+});
 export default usersRoute;
